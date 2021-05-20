a035d76410f20d8429ed079835c3b15519008baa
Dnyanesh Mohite <dmohite@gmail.com>
2021-05-20 19:57:40 +0530

    Fix Dynamic Partition

--- priv-app/OPLauncher/Android.mk
+++ priv-app/OPLauncher/Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE_STEM := OPLauncher.apk
 LOCAL_SRC_FILES := OPLauncher.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app/OPLauncher
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/priv-app/OPLauncher
 LOCAL_OVERRIDES_PACKAGES := PixelLauncher TrebuchetQuickStep Launcher3 NexusLauncherPrebuilt NexusLauncherRelease ParanoidQuickStep Launcher3QuickStep
 
 include $(BUILD_PREBUILT)
